224d0d7849dd40eb452435491bcc90246d97c750
Alexander Mauriz <alexmauriz@gmail.com>
2024-06-03 16:45:01 -0400

    updated links on app.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,7 +3,10 @@
     <header> <navigation /> </header>
     <div id="nav">
       
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;-->
+      <router-link v-bind:to="{ name: 'login'}">Login</router-link>
+      |
+      <router-link v-bind:to="{ name: 'register' }">Register</router-link>
       <router-link v-bind:to="{ name: 'logout' }" v-if="this.$store.state.token != ''">Logout</router-link>
 
     </div>
