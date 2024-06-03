e56c951ffab48644a36fb6db675a77ccc67d2228
Nathan Dickinson <nathandickinson32@gmail.com>
2024-06-03 14:14:22 -0400

    madel making, ingredient

---
+++ java/src/main/java/com/techelevator/model/Recipe.java
@@ -0,0 +1,37 @@
+package com.techelevator.model;
+
+public class Recipe {
+private int recipeId;
+private String recipeName;
+private String description;
+private int categoryId;
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+}
