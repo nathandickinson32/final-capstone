3b4db4ec48a663272339207c08b6a31def6ccdaf
Alexander Mauriz <alexmauriz@gmail.com>
2024-06-04 11:57:26 -0400

    minor changed to navigation

--- vue/src/components/BLDImages.vue
+++ vue/src/components/BLDImages.vue
@@ -24,7 +24,7 @@
 
     
 <style>
-img{
+img {
     float: left;
     width:  65vh;
     height: 65vh;
--- vue/src/components/navigation.vue
+++ vue/src/components/navigation.vue
@@ -54,7 +54,8 @@ const offScreenMenu = document.querySelector('.off-screen-menu');
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
-})}
+})
+}
 };
 
 
@@ -179,4 +180,6 @@ nav {
 }
 
 </style>
+
+
 
