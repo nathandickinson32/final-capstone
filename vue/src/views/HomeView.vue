e4ead30671511cc054751e59f5a9eb4bd9a7005c
Sean McMann <seanmcm@udel.edu>
2024-06-04 11:20:59 -0400

    Adding images, css and html for links

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,6 +2,26 @@
   <div>
     <Navigation />
   </div>
+  <div>
+    <!--add images and turn them into links -->
+    <a href=""> 
+        <img src=
+        "omelet.jpg"
+             alt=""> 
+    </a>
+ 
+  <a href=""> 
+        <img src=
+        "cheeseburger_stock.jpg"
+             alt=""> 
+    </a>
+
+    <a href=""> 
+        <img src="Chicken-Parmesan-1.jpg"
+             alt=""> 
+    </a>
+  </div>
+  
 </template>
 
 <script>
@@ -11,4 +31,19 @@ export default {
     components: { Navigation }
 };
 </script>
+
+<style>
+img{
+    float: left;
+    width:  65vh;
+    height: 65vh;
+    object-fit: cover;
+    
+    
+}</style>
+
+
+
+
+
 
