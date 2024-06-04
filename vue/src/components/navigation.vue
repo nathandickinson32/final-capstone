ccb8b27a1542811e792c2ffc44b1f46bcf07082c
Alexander Mauriz <alexmauriz@gmail.com>
2024-06-04 11:33:15 -0400

    trying to fix issue with static nav

--- vue/src/components/navigation.vue
+++ vue/src/components/navigation.vue
@@ -46,13 +46,17 @@
 </template>
 
 <script>
+export default {
+created() {
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
-})
+})}
+};
+
 
 </script>
 
