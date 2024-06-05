97f3a5a44681395e22adce6a0aa68d188e6c584c
Dinda O'Brien <dinda.obrien@gmail.com>
2024-06-05 13:35:06 -0400

    added recipe_users table

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS recipe_users;
 DROP TABLE IF EXISTS recipe_instructions;
 DROP TABLE IF EXISTS recipe_categories;
 DROP TABLE IF EXISTS recipe_ingredients;
@@ -30,10 +31,7 @@ recipe_name varchar NOT NULL,
 description varchar NOT NULL,
 author_id int NOT NULL,
 CONSTRAINT PK_recipe_id PRIMARY KEY(recipe_id)
-
 );
-
-
 
 CREATE TABLE recipe_categories (
 recipe_id int NOT NULL,
@@ -65,168 +63,11 @@ CONSTRAINT PK_instructions_id PRIMARY KEY(instructions_id),
 CONSTRAINT FK_recipe_id FOREIGN KEY(recipe_id) REFERENCES recipe(recipe_id)
 );
 
-INSERT INTO categories(category_type, category_name)
-VALUES
-('Time', 'Breakfast'),
-('Time', 'Lunch'),
-('Time', 'Dinner'),
-('Diet', 'High Protein'),
-('Diet', 'Low-Carb'),
-('Diet', 'Vegetarian/Vegan'),
-('Flavor', 'Asian'),
-('Flavor', 'Italian'),
-('Flavor', 'Mexican'),
-('Flavor', 'Middle-Eastern');
-
-INSERT INTO recipe(recipe_name, description, author_id)
-VALUES
-('Stir-Fry', 'Stir & Fry', 1),
-('Fries', 'fry em', 2);
-
-INSERT INTO recipe_categories(recipe_id, category_id)
-VALUES
-(1,1),
-(2,2);
-
-
-
-
-INSERT INTO ingredients(ingredient_name)
-VALUES
-
-('Carrots'),
-('Potatoes'),
-('Onions'),
-('Garlic'),
-('Bell peppers '),
-('Tomatoes'),
-('Spinach'),
-('Kale'),
-('Broccoli'),
-('Cauliflower'),
-('Zucchini'),
-('Eggplant'),
-('Mushrooms'),
-('Cucumbers'),
-('Lettuce'),
-('Avocados'),
-('Corn'),
-('Apples'),
-('Bananas'),
-('Oranges'),
-('Strawberries'),
-('Blueberries'),
-('Grapes'),
-('Lemons'),
-('Limes'),
-('Pineapples'),
-('Mangoes'),
-('Peaches'),
-('Cherries'),
-('Watermelon'),
-('Kiwi'),
-('Rice'),
-('Quinoa'),
-('Oats'),
-('Barley'),
-('Wheat flour'),
-('Cornmeal'),
-('Couscous'),
-('Bulgur'),
-('Chickpeas'),
-('Lentils'),
-('Beans'),
-('Peas'),
-('Milk'),
-('Cheese'),
-('Yogurt'),
-('Butter'),
-('Cream'),
-('Eggs'),
-('Chicken'),
-('Beef'),
-('Pork'),
-('Lamb'),
-('Turkey'),
-('Seafood'),
-('Salmon'),
-('Tuna'),
-('Shrimp'),
-('Cod'),
-('Scallops'),
-('Crab'),
-('Lobster'),
-('Salt'),
-('Black pepper'),
-('Cumin'),
-('Paprika'),
-('Turmeric'),
-('Cinnamon'),
-('Nutmeg'),
-('Oregano'),
-('Basil'),
-('Thyme'),
-('Rosemary'),
-('Parsley'),
-('Cilantro'),
-('Olive oil'),
-('Vegetable oil'),
-('Coconut oil'),
-('Butter'),
-('Ghee'),
-('Soy sauce'),
-('Ketchup'),
-('Mustard'),
-('Mayonnaise'),
-('Hot sauce'),
-('BBQ sauce'),
-('Vinegar'),
-('Honey'),
-('Maple syrup'),
-('Flour'),
-('Sugar'),
-('Baking powder'),
-('Baking soda'),
-('Yeast'),
-('Vanilla extract'),
-('Cocoa powder'),
-('Chocolate chips'),
-('Almonds'),
-('Walnuts'),
-('Cashews'),
-('Peanuts'),
-('Sunflower seeds'),
-('Chia seeds'),
-('Flax seeds'),
-('Pumpkin seeds'),
-('Beverages'),
-('Coffee'),
-('Tea'),
-('Juice'),
-('Soda'),
-('Water'),
-('Wine'),
-('Beer'),
-('Miscellaneous'),
-('Bread'),
-('Pasta'),
-('Canned goods (tomatoes, beans, corn)'),
-('Broth (chicken, beef, vegetable)'),
-('Tofu'),
-('Miso paste');
-
-INSERT INTO recipe_ingredients(recipe_id, ingredient_id)
-VALUES
-(1,1),
-(2,2),
-(2,5);
-
-INSERT INTO recipe_instructions(step, instruction, recipe_id)
-VALUES
-(1,'get fries', 2),
-(2,'fyr em', 2),
-(3,'eat them while they are hot!', 2);
-
-
+CREATE TABLE recipe_users(
+recipe_id int NOT NULL,
+user_id int NOT NULL,
+CONSTRAINT FK_recipe_id FOREIGN KEY(recipe_id) REFERENCES recipe(recipe_id),
+CONSTRAINT FK_user_id FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
 
 COMMIT TRANSACTION;
