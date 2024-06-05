956da799f2b4dfe0486bafd895fe309ee0f0e2b5
Sean McMann <seanmcm@udel.edu>
2024-06-05 13:54:49 -0400

    adding background text for images

--- vue/src/components/BLDImages.vue
+++ vue/src/components/BLDImages.vue
@@ -3,23 +3,24 @@
     <div>
     <!--add images and turn them into links -->
     <a href="/breakfast"> 
-        <img src=
-        "/oatmeal2-1-of-1.webp"
-             alt=""> 
+        <img src="/oatmeal2-1-of-1.webp" alt="oatmeal"> 
+        <div class="top-center-text">Breakfast</div>
     </a>
- 
+    
+        </div>
   <router-link v-bind:to="{name: 'category', params: {id: 2}}"> 
-        <img class = "burger" src="cheeseburger_stock.jpg" alt=""> 
+        <img class = "burger" src="cheeseburger_stock.jpg" alt="cheeseburger"> 
+        <div class="top-center-lunch">Lunch</div>
              
   </router-link>
   
 
 
     <a href=""> 
-        <img src="Chicken-Parmesan-1.jpg"
-             alt=""> 
+        <img src="Chicken-Parmesan-1.jpg" alt="chicken parm"> 
+        <div class="top-center-dinner">Dinner</div>
     </a>
-     </div>
+    <!--</div>-->
 
     </template>
 
@@ -42,6 +43,32 @@ img {
     border-left: none;
     border-right: none;
 
+}
+
+.top-center-text{
+  position: absolute;
+  top: 45%;
+  left: 17%;
+  transform: translate(-50%, -50%);
+  font-size: 3rem;
+  color:hotpink;
+}
+
+.top-center-lunch{
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 3rem;
+  color:hotpink;
+}
+.top-center-dinner{
+  position: absolute;
+  top: 45%;
+  left: 83%;
+  transform: translate(-50%, -50%);
+  font-size: 3rem;
+  color:hotpink;
 }</style>
 
 
