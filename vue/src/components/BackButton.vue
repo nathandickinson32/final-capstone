71b6ddaedc649b43e145523a5151852bc2d7d185
Nathan Dickinson <nathandickinson32@gmail.com>
2024-06-07 11:59:47 -0400

    back button success, star needs work

---
+++ vue/src/components/BackButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="backButtonFormat"> 
+        <div class="backButton">
+            <button v-on:click="this.$router.back()">Back</button>
+        </div>
+    </div>
+   
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.backButton {
+    background-image: url(../images/back.png);
+}
+
+.backButtonFormat{
+    width: 50px;
+  height: 50px;
+  border: none;
+  margin: 0;
+  background-repeat: no-repeat;
+  background-size: 40px 40px;
+}
+</style>
