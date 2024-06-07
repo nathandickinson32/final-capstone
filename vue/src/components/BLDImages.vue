cba9ebc67509316af985b798900b1766a9d88fd7
Alexander Mauriz <alexmauriz@gmail.com>
2024-06-07 16:07:24 +0000

    Replace BLDImages.vue

--- vue/src/components/BLDImages.vue
+++ vue/src/components/BLDImages.vue
@@ -1,6 +1,13 @@
 
 <template>
-    <div>
+
+    
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <div class="container">
+    <div id="breakfast">
     <!--add images and turn them into links -->
     <router-link v-bind:to="{name: 'category', params: {id: 1}}"> 
 
@@ -11,19 +18,19 @@
     
         </div>
 
-        <div>
+        <div id="lunch">
   <router-link v-bind:to="{name: 'category', params: {id: 2}}"> 
         
-        <img class = "burger" id="big3" src="cheeseburger_stock.jpg" alt="cheeseburger"> 
+        <img  id="big3" src="cheeseburger_stock.jpg" alt="cheeseburger"> 
         <div class="top-center-lunch">Lunch</div>
-
+        
              
   </router-link>
 
-</div>
+        </div>
   
 
-<div>
+<div id="dinner">
     <router-link v-bind:to="{name: 'category', params: {id: 3}}"> 
         <img id="big3" src="Chicken-Parmesan-1.jpg" alt="chicken parm"> 
         <div class="top-center-dinner">Dinner</div>
@@ -31,28 +38,48 @@
 
         
 </div>
+</div>
     
 
     </template>
 
     
 <style scoped>
+.container {
+    display: grid;
+    /*grid-template-columns: 1fr 1fr 1fr;    */
+    margin-top: 10%;
+    position: relative;
+}
+#breakfast {
+    position: relative;
+    grid-area: breakfast;
+    justify-content: center;
+}
+
+#lunch {
+    position: relative;
+    grid-area: lunch;
+    justify-content: center;
+}
+
+#dinner{
+    position: relative;
+    grid-area: dinner;
+    justify-content: center;
+}
+
 #big3 {
-    float: left;
-    width:  33%;
+    
+    float: center;
+    border: solid 2px black;
+    border-radius: 10px;
+    width:  40vh;
     height: 45vh;
     object-fit: cover;
-    margin-top: 10%;
-    border: solid 1px black;
-    border-radius: 10px;
-}
-
-.burger{
     
-    border-left: none;
-    border-right: none;
-
 }
+
 
 .top-center-breakfast{
     z-index: 1;
@@ -63,7 +90,7 @@
   padding: 5px;
   top: 45%;
   left: 17%;
-  transform: translate(-50%, -50%);
+  transform: translate(0, -50%);
   font-size: 3rem;
   color:black;
 }
@@ -90,12 +117,52 @@
   padding: 5px;
   top: 45%;
   left: 83%;
-  transform: translate(-50%, -50%);
+  transform: translate(-130%, -50%);
   font-size: 3rem;
   color:black;
 }
 
 
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+  .container {
+    image-rendering: auto;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+    "breakfast"
+    "lunch"
+    "dinner";
+  }
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+  .media {
+    width: 100vh;
+  }
+}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {
+  .media {
+    width: 100vh;
+  }
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+  .media{
+    width: 100vh;
+  }
+}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+  .media {
+    width: 100vh;
+  }
+}
 
 
 </style>
