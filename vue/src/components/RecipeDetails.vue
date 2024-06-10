c40792456a22d92178d36b6597083d422be8e5b2
Dinda O'Brien <dinda.obrien@gmail.com>
2024-06-10 15:58:37 -0400

    adding more to test

--- vue/src/components/RecipeDetails.vue
+++ vue/src/components/RecipeDetails.vue
@@ -7,6 +7,7 @@
      <p>picture ?</p>
     </div> -->
     Testing testing 123
+    Testing again
     <div class='broad-details'>
       <div class='icon-holder'>
         <div v-on:click="favoriteUnfavorite(recipe.recipeId)" class='recipe icon details-icon' v-bind:class="this.idLibrary.includes(recipe.recipeId) ? 'favorite' : 'unfavorite'" v-show="this.$store.state.token != ''">
